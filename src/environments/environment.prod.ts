ede36e47e3ca190296607c573971ff93ffab5786
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-14 14:01:30 +0200

    created environment interface; added apiKey to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyB6c3zosDJ9jR-hDI6nk_idgYWfB8vNncU'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyB6c3zosDJ9jR-hDI6nk_idgYWfB8vNncU'
 };
 
 /*
