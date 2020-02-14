bdb5c56f66ed1a48a518cb57321743f7ae4b9fa9
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-14 15:35:11 +0200

    added flag submited to login form

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -11,6 +11,7 @@ import {Router} from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup;
+  submitted = false;
 
   constructor(
     private auth: AuthService,
@@ -35,6 +36,7 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submitted = true;
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -43,6 +45,7 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
     });
 
   }
