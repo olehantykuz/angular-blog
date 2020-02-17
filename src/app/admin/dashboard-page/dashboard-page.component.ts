b60446a34b43243c51f9eef0b8c9b7209ba9defb
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-17 17:41:13 +0200

    filled dashboard page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from '../../shared/posts.service';
+import {Post} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  postsSubscription: Subscription;
+
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit() {
+    this.postsSubscription = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe();
+    }
+  }
+
+  remove(id: string) {
+
+  }
 }
