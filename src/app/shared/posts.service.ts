189155338f3308d7062d8ea6ff0b5f54c260e9b1
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-17 13:08:30 +0200

    return post after saving post in db

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Post} from './interfaces';
+import {FbCreateResponse, Post} from './interfaces';
 import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,16 @@ export class PostsService {
   ) { }
 
   create(post: Post): Observable<Post> {
-    return this.http.post<Post>(`${environment.fbDbUrl}/posts.json`, post);
+    return this.http.post(`${environment.fbDbUrl}/posts.json`, post)
+      .pipe(
+        map<FbCreateResponse, Post>((response: FbCreateResponse) => {
+          return {
+            ...post,
+            id: response.name,
+            date: new Date(post.date)
+          };
+        })
+      );
   }
 
 }
