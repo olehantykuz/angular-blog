3ef897db0d8ada5efb40fb9a16967ca7c77243c7
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-17 12:38:55 +0200

    added to environment fbDbUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyB6c3zosDJ9jR-hDI6nk_idgYWfB8vNncU'
+  apiKey: 'AIzaSyB6c3zosDJ9jR-hDI6nk_idgYWfB8vNncU',
+  fbDbUrl: 'https://angular-blog-614eb.firebaseio.com'
 };
 
 /*
