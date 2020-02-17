29bdaa15434116c5a912f121d14877b34eeb5ad2
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-17 18:38:19 +0200

    created search pipe

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,9 +8,9 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import {AuthService} from './shared/services/auth.service';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
+import {SearchPipe} from './shared/search.pipe';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import {AuthGuard} from './shared/services/auth.guard';
     LoginPageComponent,
     DashboardPageComponent,
     CreatePageComponent,
-    EditPageComponent
+    EditPageComponent,
+    SearchPipe
   ],
   imports: [
     CommonModule,
