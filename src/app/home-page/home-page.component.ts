3cddbad3b401137daae60c48539148c948db2af9
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-18 18:32:18 +0200

    displayed post from server on home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from '../shared/posts.service';
+import {Post} from '../shared/interfaces';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  posts$: Observable<Post[]>;
 
-  constructor() { }
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit() {
+    this.posts$ = this.postsService.getAll();
   }
 
 }
